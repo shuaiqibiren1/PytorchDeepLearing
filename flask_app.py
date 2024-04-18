efd8f23cb6d8431bcc43897f8027ca41bb496f31
JJzbkn <160199707+JJzbkn@users.noreply.github.com>
2024-04-18 20:18:20 +0800

    Add files via upload
    包括myvi flask 和 templates

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 from model import *
 import os
 import torch
 import SimpleITK as sitk
+import subprocess
 
 # Use CUDA
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
@@ -10,7 +11,6 @@ os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 use_cuda = torch.cuda.is_available()
 
 app = Flask(__name__)
-
 
 # 载入模型
 newSize = (112, 112, 128)
@@ -27,37 +27,55 @@ if not os.path.exists(root_Mask_dir):
     os.makedirs(root_Mask_dir)
 
 
+# 定义根路由，渲染 HTML 页面
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 # 定义服务接口
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/process', methods=['POST'])
+def process():
     file = request.files.get('file')  # 获取上传的文件
     if file:
-        file.save(root_dir + '/' + file.filename)  # 将上传文件保存到本地
-        sitk_image = sitk.ReadImage(root_dir + '/' + file.filename)  # 读取本地文件
-        sitk_mask = Unet3d.inference(sitk_image, newSize)  # 对本地文件进行推理计算
-        # 返回预测结果
-        sitk.WriteImage(sitk_mask, root_Mask_dir + '/' + file.filename)
+        # 保存上传的文件
+        file.save(root_dir + '/' + file.filename)
+        
+        # 读取上传的文件
+        sitk_image = sitk.ReadImage(root_dir + '/' + file.filename)
+        
+        # 执行推理
+        sitk_mask = Unet3d.inference(sitk_image, newSize)
+        
+        # 保存推理结果
+        sitk.WriteImage(sitk_mask, root_Mask_dir + '/' + "result.nii")
+        
         return 'Segmentation Success!'
     else:
         return 'No file uploaded'
 
-
-# 定义服务接口
-@app.route('/getresult', methods=['GET'])
-def getresult():
+# 定义下载结果文件接口
+@app.route('/download', methods=['GET'])
+def download():
     filename = request.args.get('file')  # 获取请求参数中的文件名
+    # print("The filename is:", filename)  # 打印文件名
     if not filename:
+        print("Missing parameter: file")
         return "Missing parameter: file"  # 没有提供文件名
-    filepath = root_Mask_dir + '/' + filename  # 生成完整的文件路径
+    filepath = os.path.join(root_Mask_dir + '/' + filename)  # 生成完整的文件路径
+    filepath = "C:\\Users\\admin\\Desktop\\challenge\\PytorchDeepLearing-main\\data\\uploads\\Mask" + '\\' + filename
+    print("filepath : ", filepath)
+    
     try:
-        return send_file(filepath, as_attachment=True, attachment_filename=filename)
+        return send_file(filepath) #进行了修改
     except FileNotFoundError:
         return "The file does not exist"  # 文件不存在
 
+# 定义可视化接口
+@app.route('/visualization', methods=['GET'])
+def visualization():
+    # 调用 show3d.py 进行可视化
+    subprocess.run(['python', 'show3d.py'])
+    return 'Visualization Success!'
 
 if __name__ == '__main__':
-    # 使用curl命令行工具发送基于文件的POST请求来测试服务，192.168.10.96是服务器的ip
-    # curl -X POST -F "file=@E:/challenge/api_test/TRAIN000106.nii.gz" http://127.0.0.1:8000/predict
-    # 使用curl命令行工具发送基于文件的GET请求来测试服务
-    # curl 192.168.10.96:8000/getresult?file=TRAIN000106.nii.gz -o /home/Project/TRAIN000106.nii.gz
     app.run(host='0.0.0.0', port=8000)
---
+++ show3d.py
@@ -0,0 +1,57 @@
+import nibabel as nib
+import myvi
+import numpy as np
+from dataprocess.utils import file_name_path
+import scipy.ndimage as ndimg
+
+# 加载 NIfTI 文件
+# nii = nib.load("dataset\Mask\mr_train_1002.nii")
+maskpathname = "dataset\Mask\mr_train_1002.nii"
+# 需要添加一部分来导入uploads中的数据
+# datapath = "data/uploads/Image"
+# maskpath = "data/uploads/Mask"
+# image_path_list = file_name_path(datapath, False, True)
+# maskpathname = maskpath + "/" + image_path_list[0]
+nii = nib.load(maskpathname)
+
+# nii = nib.load('myvi/data/patient2_LGE_manual.nii')
+imgs = nii.get_fdata()
+
+# 获取唯一标签值
+unique_labels = np.unique(imgs)
+
+# 打印唯一标签值
+print("Unique labels in imgs:", unique_labels)
+
+# 映射标签值到连续整数范围
+mapped_labels = np.linspace(0, len(unique_labels) - 1, len(unique_labels)).astype(int)
+
+# 获取图像空间信息
+zoom = nii.header.get_zooms()
+
+# 设置平滑参数
+sigma = 1
+
+# 创建 Manager 对象
+manager = myvi.Manager()
+
+# 预定义颜色数组
+colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1), (0, 1, 1), (0.5, 0.5, 0.5)]
+
+# 遍历唯一标签值，为每个标签值创建相应的器官表面
+for label, mapped_label in zip(unique_labels, mapped_labels):
+    # 使用 Gaussian 滤波平滑数据
+    organ = ndimg.gaussian_filter(np.float32(imgs == label), sigma)
+    
+    # 构建器官表面
+    vts, fs, ns, vs = myvi.util.build_surf3d(organ, 1, 0.5, zoom)
+    
+    # 获取对应的颜色
+    color = colors[mapped_label % len(colors)]  # 使用模运算确保颜色索引不超出范围
+    
+    # 将器官添加到 Manager 对象中
+    manager.add_surf(str(int(label)), vts, fs, ns, color)  # 将标签转换为整数作为名称，并使用独热编码的颜色
+    
+# 显示器官 3D 模型
+manager.show('Organ 3D Demo')
+
