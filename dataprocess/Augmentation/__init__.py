c61834b6688c1d4649b7663ed6d6ca7c6da540d4
Aircraft-carrier <122367292+Aircraft-carrier@users.noreply.github.com>
2024-04-18 19:54:18 +0800

    Add files via upload
    dataprocess

---
+++ dataprocess/Augmentation/__init__.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+__author__ = 'junqiang chen'
+__version__ = '1.0.0'
+__time__ = '2019.2.19'
+=======
+__author__ = 'junqiang chen'
+__version__ = '1.0.0'
+__time__ = '2019.2.19'
+>>>>>>> 4249ea4ddcbb046cdaa9175b7305dcecf8feeaf2
